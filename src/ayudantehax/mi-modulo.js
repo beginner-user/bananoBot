211d54a05d2ca144692a9b239551f68366c04955
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-20 19:46:57 -0300

    Project set-up
    * Create tú-modulo.js
    
    * Create mi-modulo.js
    
    * Delete src/Anonjoy directory
    
    * Create tu-modulo.js
    
    * Create repository.json
    
    * Create default.js
    
    * Update default.js
    
    * Create development.js
    
    * Update repository.json
    
    * Update tu-modulo.js
    
    * Update mi-modulo.js
    
    * Update default.js
    
    * Update development.js
    
    * Update development.js

---
+++ src/ayudantehax/mi-modulo.js
@@ -0,0 +1,24 @@
+
+/* * Plugin specification * */
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `ayudantehax/mi-modulo`,
+  author: `ayudantehax`,
+  version: `1.0.0`,
+  config: {
+    param1: `value`,
+  },
+  configDescriptions: {
+    param1: `Description`,
+  },
+  dependencies: [`aut/otherPlugin1`],
+  order: {
+    'onPlayerChat': {
+      'before': [`aut/otherPlugin1`, `aut/otherPlugin2`],
+      'after': [`aut/otherPlugin3`],
+    }
+  },
+  incompatible_with: [`aut/otherPlugin4`],
+}
