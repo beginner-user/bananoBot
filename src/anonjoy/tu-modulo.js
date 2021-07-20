1ff52366c98217846a8f80e968e9afde628f57d9
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-20 20:20:28 -0300

    Corrections

--- src/anonjoy/tu-modulo.js
+++ src/anonjoy/tu-modulo.js
@@ -9,16 +9,5 @@ room.pluginSpec = {
   version: `1.0.0`,
   config: {
     param1: `value`,
-  },
-  configDescriptions: {
-    param1: `Description`,
-  },
-  dependencies: [`aut/otherPlugin1`],
-  order: {
-    'onPlayerChat': {
-      'before': [`aut/otherPlugin1`, `aut/otherPlugin2`],
-      'after': [`aut/otherPlugin3`],
-    }
-  },
-  incompatible_with: [`aut/otherPlugin4`],
+  }
 }
