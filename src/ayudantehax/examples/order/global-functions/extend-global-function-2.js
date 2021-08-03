a7e7768f1791705ce4ad3cf2c1f21f230222e918
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-03 02:51:35 -0300

    Create extend-global-function-2.js

---
+++ src/ayudantehax/examples/order/global-functions/extend-global-function-2.js
@@ -0,0 +1,27 @@
+
+/* * Plugin specification * */
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `ayudantehax/examples/order/global-functions/extend-global-function-2`,
+  author: `ayudantehax`,
+  order: {
+    'sendAnnouncement': {
+      'after': [`ayudantehax/examples/order/global-functions/extend-global-function-1`],
+    },
+  },
+}
+
+function secondExtension({ previousFunction, callingPluginName }, ...args) {
+  console.log(`Last extension first call, from the plugin ` + callingPluginName);
+  if (typeof previousFunction === `function`) {
+    return previousFunction(...args);
+  }
+}
+
+function onRoomLinkHandler(url) {
+  room.extend(`sendAnnouncement`, secondExtension);
+}
+
+room.onRoomLink = onRoomLinkHandler;
