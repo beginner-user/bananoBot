c94b8582ca44c331364954ad93af53e4e5495b2d
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-03 18:35:08 -0300

    Update import-function-without-depency.js

--- src/ayudantehax/examples/import-export-functions/import-function-without-depency.js
+++ src/ayudantehax/examples/import-export-functions/import-function-without-depency.js
@@ -1,1 +1,18 @@
 
+/* * Plugin specification * */
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `ayudantehax/examples/import-export-functions/import-function-without-depency`,
+  author: `ayudantehax`,
+}
+
+const pluginName = `ayudantehax/examples/import-export-functions/export-function`;
+
+function someFunction() {
+  if (room.hasPlugin(pluginName)) {
+    let importedFunction = room.getPlugin(pluginName).publicFunction;
+    // do something with the imported function ...
+  }
+}
