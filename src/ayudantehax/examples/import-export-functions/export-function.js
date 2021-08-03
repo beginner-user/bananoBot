872e240962afb57930134c01d9962914d0c1f84c
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-03 17:33:23 -0300

    Create export-function.js

---
+++ src/ayudantehax/examples/import-export-functions/export-function.js
@@ -0,0 +1,22 @@
+/* * Plugin specification * */
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `ayudantehax/examples/import-export-functions/export-function`,
+  author: `ayudantehax`,
+}
+
+function privateFunction() { // no fue exportada, asi que no podrá ser llamada
+  console.log(`Private function has been called`); 
+  // ...
+}
+
+function publicFunction() {
+  console.log(`Public function has been called`);
+  // ...
+}
+
+// Exportar función
+
+room.publicFunction = publicFunction;
