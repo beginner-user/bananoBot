0e3dcd34efabcfed037e25b9cbb88ca96a62f084
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-03 21:51:11 -0300

    Update pre-event-handler-hook.js

--- src/ayudantehax/examples/handling-events/pre-event-handler-hook.js
+++ src/ayudantehax/examples/handling-events/pre-event-handler-hook.js
@@ -1,1 +1,31 @@
 
+/* * Plugin specification * */
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `ayudantehax/examples/handling-events/pre-event-handler-hook`,
+  author: `ayudantehax`,
+  config: {
+    invalidEventState: false, // ver explicación en https://hhm.surge.sh/api/tutorial-events.html#pre-event-handler-hooks
+  },
+}
+
+const config = room.getConfig();
+
+function onSomeEventPreEventHandlerHook({ room, metadata }, ...args) {
+  console.log(`The previous event left a valid event status`);
+  // ...
+  if (config.invalidEventState === true) {
+    // ...
+    return false; // los manejadores de eventos restantes NO seran ejecutados
+  }
+  else {
+    // ...
+    return true; // los manejadores de eventos restantes seran ejecutados
+  }
+}
+
+room.onRoomLink = function(url) {
+  room.addPreEventHandlerHook(`onSomeEvent`, onSomeEventPreEventHandlerHook);
+}
