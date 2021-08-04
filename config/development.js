cee8fb064084df5a27c5798e0b1572081489f632
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-03 21:42:12 -0300

    Update development.js

--- config/development.js
+++ config/development.js
@@ -41,8 +41,8 @@ HHM.config.plugins = {
   
   'ayudantehax/examples/handling-events/event-handler-1': {},
   'ayudantehax/examples/handling-events/event-handler-2': {},
-  'ayudantehax/examples/handling-events/pre-event-handler-1': {},
-  'ayudantehax/examples/handling-events/pre-event-handler-2': {},
+  'ayudantehax/examples/handling-events/pre-event-hook-1': {},
+  'ayudantehax/examples/handling-events/pre-event-hook-2': {},
 
   // 'iniciar/modulo' : {}
 };
