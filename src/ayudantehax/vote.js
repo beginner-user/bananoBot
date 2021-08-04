72c27718d680089ecb6fdf3f882cca496fb49a41
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-04 00:50:02 -0300

    Update vote.js

--- src/ayudantehax/vote.js
+++ src/ayudantehax/vote.js
@@ -6,3 +6,22 @@ room.pluginSpec = {
   name: `ayudantehax/vote`,
   author: `ayudantehax`,
 }
+
+async function playerVote(playerId) {
+  room.sendAnnouncement(`escribi algo`, playerId);
+  let response = await room.getPlayerInput(playerId);
+  if (response !== false) {
+    room.sendAnnouncement(`escribiste: ` + response, playerId);
+  }
+}
+
+function playersVote() {
+  let players = room.getPlayerList().filter(p => p.id !== 0);
+  for (let player of players) {
+    playerVote(player.id);
+  }
+}
+
+// Exportar
+
+room.playersVote = playersVote;
