3258d27dec74523a1e44e06c6ed4d27a074c5107
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-16 05:06:29 +0300

    Create a.js

---
+++ src/ayudantehax/gamemode/a.js
@@ -0,0 +1,12 @@
+
+/**
+ * @description 
+ * See [explanation]{@link https://hhm.surge.sh/api/tutorial-writing-plugins.html#sav-help-display-help-and-usage-information}
+ * and [code source]{@link https://github.com/saviola777/hhm-plugins/blob/master/src/sav/help.js}
+ */
+const onCommandAuthData = {
+  'sav/chat': {
+    text: ` ROLE PASSWORD`,
+    roles: ['user']
+  }
+}
