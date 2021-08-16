01442b9aa011323bd8f4f12fc857c6bbd6426c5c
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-16 03:16:06 +0300

    Update terminate.js

--- src/ayudantehax/gamemode/terminate.js
+++ src/ayudantehax/gamemode/terminate.js
@@ -5,9 +5,15 @@ room.pluginSpec = {
   author: `ayudantehax`
 }
 
+/**
+ * @param {HhmRoomObject} plugin
+ * @description https://hhm.surge.sh/api/tutorial-events.html#hhm-events
+ *              https://hhm.surge.sh/api/HHM.events.html
+ */
 function onHhmPluginRemovedHandler(plugin) {
   switch (plugin.getName()) {
     case `ayudantehax/gamemode/ctf/core`:
+      // reset avatars at the end of gamemode ctf
       room.getPlayerList().forEach((player) => {
         room.setPlayerAvatar(player.id, null);
       });
