51c9f70ab2a78c600be4f8ee851c5cf0ca555fce
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-16 05:31:46 +0300

    Update init.js

--- src/ayudantehax/gamemode/ctf/init.js
+++ src/ayudantehax/gamemode/ctf/init.js
@@ -5,4 +5,23 @@ room.pluginSpec = {
   author: `ayudantehax`
 }
 
+//
+// Event Handlers
+//
 
+function onRoomLinkHandler() {
+  room.getPlayerList().forEach((player) => {
+    room.setPlayerAvatar(player.id, `\u2000`);
+  });
+}
+
+function onPlayerJoinHandler(player) {
+  room.setPlayerAvatar(player.id, `\u2000`);
+}
+
+//
+// Exports
+//
+
+room.onRoomLink = onRoomLinkHandler;
+room.onPlayerJoin = onPlayerJoinHandler;
