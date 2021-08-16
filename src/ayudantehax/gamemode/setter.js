fc5dfe4f9f24e178e46c3392665962de8bfcdc8a
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-16 03:45:09 +0300

    Create setter.js

---
+++ src/ayudantehax/gamemode/setter.js
@@ -0,0 +1,68 @@
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `ayudantehax/gamemode/setter`,
+  author: `ayudantehax`,
+  dependencies: [
+    `sav/plugin-control`
+  ],
+}
+
+/**
+ * @type {string}
+ */
+let currentGamemode;
+
+/**
+ * @type {HhmRoomObject}
+ */
+let pluginControl;
+
+//
+// Event Handlers
+//
+
+/**
+ * @description https://hhm.surge.sh/api/tutorial-writing-plugins.html#sav-help-display-help-and-usage-information
+ *              https://github.com/saviola777/hhm-plugins/blob/master/src/sav/help.js
+ */
+const onCommandAuthData = {
+  'sav/chat': {
+    text: ` ROLE PASSWORD`,
+    roles: ['user']
+  }
+}
+
+/**
+ * @param {string} url
+ * @description https://github.com/haxball/haxball-issues/wiki/Headless-Host#onroomlink
+ *              https://hhm.surge.sh/api/tutorial-writing-plugins.html#plugin-initialization
+ */
+function onRoomLinkHandler(url) {
+  pluginControl = room.getPlugin(`sav/plugin-control`);
+}
+
+/**
+ * @param {PlayerObject} player
+ * @param {string[]} arguments
+ * @param {string} argumentString
+ * @description https://hhm.surge.sh/api/tutorial-writing-plugins.html#sav-commands-easier-command-processing
+ *              https://github.com/saviola777/hhm-plugins/blob/master/src/sav/commands.js
+ */
+function onCommandGamemode1Handler(player, arguments, argumentString) {
+  switch (arguments[0]) {
+    case `ctf`: 
+      pluginControl.onCommand1_plugin_(player, arguments);
+      break;
+    case `free`:
+      
+      break;
+  }
+}
+
+//
+// Exports
+//
+
+room.onRoomLink = onRoomLinkHandler;
+room.onCommand1_gamemode = room.onCommand1_gm = onCommandGamemode1Handler;
