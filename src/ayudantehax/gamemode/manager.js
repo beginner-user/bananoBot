4058999cf62c5f96472b743d4aa77db1851e6ce3
beginner-user <88254675+beginner-user@users.noreply.github.com>
2021-08-16 05:25:04 +0300

    Update manager.js

--- src/ayudantehax/gamemode/manager.js
+++ src/ayudantehax/gamemode/manager.js
@@ -22,34 +22,7 @@ let roles;
 /**
  * TODO Documentation
  */
-function undoPlayerModifications() {
-  room.getPlayerList().forEach((player) => {
-    room.setPlayerAvatar(player.id, null);
-  });
-}
-
-//
-// Event Handlers
-//
-
-/**
- * See the [description]{@link https://github.com/haxball/haxball-issues/wiki/Headless-Host#onroomlink} of the native event
- * and its [modification]{@link https://hhm.surge.sh/api/tutorial-writing-plugins.html#plugin-initialization}
- */
-function onRoomLinkHandler() {
-  roles = room.getPlugin(`sav/roles`);
-}
-
-/**
- * @description Set gamemode
- * See [explanation]{@link https://hhm.surge.sh/api/tutorial-writing-plugins.html#sav-commands-easier-command-processing}
- * and [code source]{@link https://github.com/saviola777/hhm-plugins/blob/master/src/sav/commands.js}      
- */
-function onCommandGamemode1Handler(player, [gamemode] = []) {
-  const playerId = player.id;
-  if (!roles.ensurePlayerRoles(playerId, `host`, room)) {
-    return;
-  }
+function setGamemode({}, gamemode) {
   // check if gamemode and current gamemode are differents
   if (gamemode !== currentGamemode) {
     // check if gamemode exists.
@@ -79,6 +52,45 @@ function onCommandGamemode1Handler(player, [gamemode] = []) {
   } else {
     // error handler
   }
+}
+
+/**
+ * TODO Documentation
+ */
+function undoPlayerModifications() {
+  room.getPlayerList().forEach((player) => {
+    room.setPlayerAvatar(player.id, null);
+  });
+}
+
+//
+// Event Handlers
+//
+
+/**
+ * See the [description]{@link https://github.com/haxball/haxball-issues/wiki/Headless-Host#onroomlink} of the native event
+ * and its [modification]{@link https://hhm.surge.sh/api/tutorial-writing-plugins.html#plugin-initialization}
+ */
+function onRoomLinkHandler() {
+  // https://hhm.surge.sh/api/HhmRoomObject.html#.getPlugin
+  // https://hhm.surge.sh/api/tutorial-writing-plugins.html#sav-roles-role-and-group-management
+  // https://github.com/saviola777/hhm-plugins/blob/master/src/sav/roles.js
+  roles = room.getPlugin(`sav/roles`);
+  // https://hhm.surge.sh/api/tutorial-writing-plugins.html#exporting-functions
+  room.extend(`setGamemode`, setGamemode);
+}
+
+/**
+ * @description Set gamemode
+ * See [explanation]{@link https://hhm.surge.sh/api/tutorial-writing-plugins.html#sav-commands-easier-command-processing}
+ * and [code source]{@link https://github.com/saviola777/hhm-plugins/blob/master/src/sav/commands.js}      
+ */
+function onCommandGamemode1Handler(player, [gamemode] = []) {
+  const playerId = player.id;
+  if (!roles.ensurePlayerRoles(playerId, `host`, room)) {
+    return;
+  }
+  room.setGamemode(gamemode);
 }
 
 //
